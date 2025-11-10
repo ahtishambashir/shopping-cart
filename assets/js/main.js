117a8094776d33246a68e50f147a54fd251d3cf9
Ahtisham Bashir <ahtishamdoggar@gmail.com>
2025-11-11 00:41:56 +0500

    added the total calculation

--- assets/js/main.js
+++ assets/js/main.js
@@ -138,11 +138,17 @@ class ShoppingCart {
   }
 
   calculateTaxes(amount) {
-    return (this.taxRate / 100) * amount
+    return parseFloat(((this.taxRate / 100) * amount).toFixed(2));
   }
 
   calculateTotal() {
     const subTotal = this.items.reduce((total, item) => total + item.price, 0);
+    const tax = this.calculateTaxes(subTotal);
+    this.total = subTotal + tax;
+    cartSubTotal.textContent = `$${subTotal.toFixed(2)}`;
+    cartTaxes.textContent = `$${tax.toFixed(2)}`;
+    cartTotal.textContent = `$${this.total.toFixed(2)}`;
+    return this.total;
   }
 };
 
